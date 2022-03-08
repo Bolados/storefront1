2d5e3336a9faec19d1fa56013df7c29d71e3c720
Bolados <ebdossou@gmail.com>
2022-03-08 02:37:14 +0100

    Update settings.py

--- storefront/settings.py
+++ storefront/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-=^11ha=x99y3^s&)m02&la*2mhytr#z_g!b_go7xu3be#alsqo"
+SECRET_KEY = "######"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
